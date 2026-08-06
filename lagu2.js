92d02e227bcc2ee94e307b53f7c517855deff38d
fathiraljabbar271-cloud <fathiraljabbar271@gmail.com>
2026-08-06 17:45:31 +0700

    Create lagu2.js

---
+++ lagu2.js
@@ -0,0 +1,39 @@
+window.DAFTAR_LAGU = [
+  {
+    judul: "Untungnya, Hidup Harus Tetap Berjalan",
+    penyanyi: "Bernadya",
+    search: "untungnya hidup harus tetap berjalan bernadya",
+    chord: "[Intro]\nC G/B Am F\n\n[Verse 1]\nC         G/B\nAda masa-masa\nAm                 F\nAku merasa dunia berhenti\nC             G/B\nUntungnya bumi tetap berputar\nAm             F\nUntungnya hidup harus tetap berjalan\n\n[Chorus]\nC              G/B\nUntungnya aku tak menyerah\nAm             F\nUntungnya aku tetap bertahan\n\n[Outro]\nC G/B Am F C"
+  },
+  {
+    judul: "Gala Bunga Matahari",
+    penyanyi: "Sal Priadi",
+    search: "gala bunga matahari sal priadi",
+    chord: "[Intro]\nG C G C\n\n[Verse 1]\nG               C\nMungkinkah kau ada di sana?\nG               C\nDi antara bunga matahari yang mekar\n\n[Chorus]\nG             Em\nBila nanti saatnya tiba\nC             D\nKan ku bawakan bunga untukmu\n\n[Outro]\nG C G C"
+  },
+  {
+    judul: "Sialan",
+    penyanyi: "Adrian Khalif feat. Juicy Luicy",
+    search: "sialan adrian khalif juicy luicy",
+    chord: "[Intro]\nF G Em Am\n\n[Verse 1]\nF            G\nSialan ku bertemu lagi\nEm           Am\nDengan bayangmu di tempat ini\n\n[Chorus]\nF              G\nKenapa harus kamu lagi?\nEm             Am\nYang muncul di pikiran ini\n\n[Outro]\nF G Em Am F"
+  },
+  {
+    judul: "Sial",
+    penyanyi: "Mahalini",
+    search: "sial mahalini",
+    chord: "[Intro]\nAm F C G\n\n[Verse 1]\nAm             F\nSampai saat ini ku tak menyangka\nC              G\nKau lakukan ini pada diriku\n\n[Chorus]\nAm            F\nSial... sialnya aku\nC             G\nMempercayai semua janjimu\n\n[Outro]\nAm F C G"
+  },
+  {
+    judul: "Lantas",
+    penyanyi: "Juicy Luicy",
+    search: "lantas juicy luicy",
+    chord: "[Intro]\nC G/B Am F\n\n[Verse 1]\nC            G/B\nLantas mengapa ku masih menaruh hati?\nAm           F\nPadahal ku tahu kau sudah ada yang punya\n\n[Chorus]\nC             G/B\nLantas ku harus bagaimana?\nAm            F\nBila hatiku memilihmu\n\n[Outro]\nC G/B Am F C"
+  },
+  {
+    judul: "Komang",
+    penyanyi: "Raim Laode",
+    search: "komang raim laode",
+    chord: "[Intro]\nG D Em C\n\n[Verse 1]\nG            D\nSebab kau terlalu indah\nEm           C\nBagi kataku yang biasa\n\n[Chorus]\nG               D\nSebab kau terlalu indah untuk jadi nyata\nEm              C\nBisa memilikimu adalah keajaiban\n\n[Outro]\nG D Em C G"
+  }
+];
+    
