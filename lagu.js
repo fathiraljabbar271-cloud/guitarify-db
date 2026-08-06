16236e7c99b4797475ed54f656fb45053e28435a
fathiraljabbar271-cloud <fathiraljabbar271@gmail.com>
2026-08-06 17:15:23 +0700

    Update lagu.js

--- lagu.js
+++ lagu.js
@@ -22,6 +22,66 @@ window.DAFTAR_LAGU = [
     penyanyi: "Adrian Khalif feat. Juicy Luicy",
     search: "sialan adrian khalif juicy luicy",
     chord: `[Intro]\nF G Em Am\n\n[Verse 1]\nF            G\nSialan ku bertemu lagi\nEm           Am\nDengan bayangmu di tempat ini\n\n[Chorus]\nF              G\nKenapa harus kamu lagi?\nEm             Am\nYang muncul di pikiran ini\n\n[Outro]\nF G Em Am F`
+  },
+  {
+    judul: "Sial",
+    penyanyi: "Mahalini",
+    search: "sial mahalini",
+    chord: `[Intro]\nAm F C G\n\n[Verse 1]\nAm             F\nSampai saat ini ku tak menyangka\nC              G\nKau lakukan ini pada diriku\n\n[Chorus]\nAm            F\nSial... sialnya aku\nC             G\nMempercayai semua janjimu\n\n[Outro]\nAm F C G`
+  },
+  {
+    judul: "Lantas",
+    penyanyi: "Juicy Luicy",
+    search: "lantas juicy luicy",
+    chord: `[Intro]\nC G/B Am F\n\n[Verse 1]\nC            G/B\nLantas mengapa ku masih menaruh hati?\nAm           F\nPadahal ku tahu kau sudah ada yang punya\n\n[Chorus]\nC             G/B\nLantas ku harus bagaimana?\nAm            F\nBila hatiku memilihmu\n\n[Outro]\nC G/B Am F C`
+  },
+  {
+    judul: "Nanti Kita Seperti Ini",
+    penyanyi: "Batas Senja",
+    search: "nanti kita seperti ini batas senja",
+    chord: `[Intro]\nC G Am F\n\n[Verse 1]\nC           G\nBayangkan jika nanti\nAm          F\nKita menua bersama\n\n[Chorus]\nC              G\nNanti kita seperti ini\nAm             F\nSaling memeluk di kala dingin\n\n[Outro]\nC G Am F C`
+  },
+  {
+    judul: "Runtut",
+    penyanyi: "Feby Putri feat. Fiersa Besari",
+    search: "runtut feby putri fiersa besari",
+    chord: `[Intro]\nC F C F\n\n[Verse 1]\nC               F\nTak perlu berpura-pura baik saja\nC               F\nBila lukamu memang terasa perih\n\n[Chorus]\nC              F\nKita manusia biasa\nAm             G\nYang berhak merasa lelah\n\n[Outro]\nC F C F C`
+  },
+  {
+    judul: "Tak Segampang Itu",
+    penyanyi: "Anggi Marito",
+    search: "tak segampang itu anggi marito",
+    chord: `[Intro]\nC G/B Am F\n\n[Verse 1]\nC            G/B\nTahu kah kamu betapa sulitnya\nAm           F\nMelupakan semua kenangan kita?\n\n[Chorus]\nC              G/B\nTak segampang itu ku rasa\nAm             F\nUntuk menghapus namamu dari hati\n\n[Outro]\nC G/B Am F C`
+  },
+  {
+    judul: "Pesan Terakhir",
+    penyanyi: "Lyodra",
+    search: "pesan terakhir lyodra",
+    chord: `[Intro]\nG D/F# Em C\n\n[Verse 1]\nG             D/F#\nGenggam tanganku untuk yang terakhir\nEm            C\nPeluk tubuhku sebelum kau pergi\n\n[Chorus]\nG              D/F#\nGenggam tanganku...\nEm             C\nSimpan kenangan kita di hatimu\n\n[Outro]\nG D/F# Em C G`
+  },
+  {
+    judul: "Rumah Singgah",
+    penyanyi: "Fabio Asher",
+    search: "rumah singgah fabio asher",
+    chord: `[Intro]\nC G/B Am F\n\n[Verse 1]\nC           G/B\nMengapa kau datang bila hanya\nAm          F\nJadikan ku rumah singgahmu?\n\n[Chorus]\nC              G/B\nAku bukan rumah singgah\nAm             F\nTempatmu melepas lelah saja\n\n[Outro]\nC G/B Am F C`
+  },
+  {
+    judul: "Komang",
+    penyanyi: "Raim Laode",
+    search: "komang raim laode",
+    chord: `[Intro]\nG D Em C\n\n[Verse 1]\nG            D\nSebab kau terlalu indah\nEm           C\nBagi kataku yang biasa\n\n[Chorus]\nG               D\nSebab kau terlalu indah untuk jadi nyata\nEm              C\nBisa memilikimu adalah keajaiban\n\n[Outro]\nG D Em C G`
+  },
+  {
+    judul: "Somebody's Pleasure",
+    penyanyi: "Aziz Hedra",
+    search: "somebodys pleasure aziz hedra",
+    chord: `[Intro]\nAm F C G\n\n[Verse 1]\nAm           F\nSoul try to figure out\nC            G\nWhat is going on in my head\n\n[Chorus]\nAm             F\nI just wanna be somebody's pleasure\nC              G\nI just wanna be somebody's world\n\n[Outro]\nAm F C G`
+  },
+  {
+    judul: "Kata Mereka Ini Berlebihan",
+    penyanyi: "Bernadya",
+    search: "kata mereka ini berlebihan bernadya",
+    chord: `[Intro]\nG D/F# Em C\n\n[Verse 1]\nG            D/F#\nKu lakukan segalanya\nEm             C\nHanya untuk membuatmu bahagia\n\n[Chorus]\nG             D/F#\nApakah salah jika ku cinta?\nEm            C\nApakah salah jika ku beri semua?\n\n[Outro]\nG D/F# Em C G`
   }
 ];
     
